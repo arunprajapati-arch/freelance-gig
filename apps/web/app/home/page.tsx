8662bedce1402e8be75168439ccf5c2022ba2755
arunprajapati-arch <arunprajapati.mail@gmail.com>
2025-06-03 18:12:41 +0530

    db-init & auth-init

---
+++ apps/web/app/home/page.tsx
@@ -0,0 +1,16 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+export default async function Home() {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    
+  return (
+    <div>
+      <h1>Home</h1>
+      {JSON.stringify(session)}
+    </div>
+  )
+}
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,13 @@
+import { prisma } from "@redb/db";
+import SignIn from "@/components/sign-in";
 
-
-
-
-export default function Home() {
+export default async function Home() {
+  await prisma.$connect();
+  console.log("Connected to database");
   return (
-   <div>
-    <h1>Hello World</h1>
-   </div>
-  );
+    <div>
+      <h1>Hello World</h1>
+      <SignIn />
+    </div>
+  )
 }
